5cbcae67a095512dda9ca04715b3c185656f10c7
olafbado <niebiel1@gmail.com>
2024-04-20 12:04:41 +0200

    Add footer

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-gray-800 text-white py-1 lg:!py-2 fixed bottom-0 w-full">
+      <div className="container mx-auto text-center text-xs">
+        <p>&copy; 2024 Centrum Medyczne. Wszelkie prawa zastrzeżone.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
