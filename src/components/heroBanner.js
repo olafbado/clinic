780ec76c27dbac0efc17f451b3cb1fdf06906d76
olafbado <niebiel1@gmail.com>
2024-04-20 12:04:30 +0200

    Add heroBanner

---
+++ src/components/heroBanner.js
@@ -0,0 +1,37 @@
+import React from "react";
+import logo from "../assets/heroBanner.jpg";
+import { Link } from "react-router-dom";
+
+function HeroBanner() {
+  return (
+    <div className="bg-gradient-to-r from-green-500 via-green-300 to-green-500 py-16 text-white">
+      <div className="container mx-auto flex flex-col-reverse lg:flex-row items-center">
+        <div className="flex-1 mb-8 lg:mb-0 lg:mr-8">
+          <img
+            src={logo}
+            alt="Przychodnia HeroBanner"
+            className="w-full rounded-3xl mt-8"
+          />
+        </div>
+        <div className="flex-1 text-center lg:text-left text-black">
+          <h1 className="text-3xl lg:text-4xl font-bold mb-4">
+            Profesjonalne Centrum Medyczne
+          </h1>
+          <p className="text-lg lg:text-xl mb-8">
+            Pacjenci mogą u nas skorzystać z konsultacji lekarskich, terapii
+            fizjoterapeutycznej, wykonać badania laboratoryjne, obliterację
+            teleangiektazji żylnych (pajączków), badania USG, UKG (echo serca)
+            czy EKG.
+          </p>
+          <Link
+            to="/"
+            className="bg-white text-green-500 py-2 px-6 rounded-full text-lg font-bold uppercase hover:border-gray-800 border-3"
+          >
+            Harmonogram Przyjęć
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default HeroBanner;
