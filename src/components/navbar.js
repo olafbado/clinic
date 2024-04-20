73eb734edc57f4adbba96484b4cfaaf3c8d5db56
olafbado <niebiel1@gmail.com>
2024-04-20 12:03:57 +0200

    Add navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,29 +1,50 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
-function TopNavigation() {
+function MainNavbar() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar bg="light" expand="lg" className="!bg-green-500 p-3">
       <Container>
-        <Navbar.Brand href="#home">Centrum Medyczne</Navbar.Brand>
+        <Navbar.Brand href="#">
+          <div className="flex items-center">
+            <FontAwesomeIcon
+              icon={faHeart}
+              className="heart-icon w-[25px] h-[25px]"
+            />
+            <span className="text-3xl ml-3 font-bold">Centrum Medyczne</span>
+          </div>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+        <Navbar.Collapse
+          id="basic-navbar-nav"
+          className="flex justify-center text-xl font-semibold"
+        >
+          <Nav className="flex lg:gap-14">
+            <Nav.Link
+              as={Link}
+              to="/"
+              className="hover:underline decoration-gray-800"
+            >
+              O Nas
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/"
+              className="hover:underline decoration-gray-800"
+            >
+              Harmonogram Przyjęć
+            </Nav.Link>
+            <Nav.Link
+              as={Link}
+              to="/"
+              className="hover:underline decoration-gray-800"
+            >
+              Kontakt
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
@@ -31,4 +52,4 @@ function TopNavigation() {
   );
 }
 
-export default TopNavigation;
+export default MainNavbar;
